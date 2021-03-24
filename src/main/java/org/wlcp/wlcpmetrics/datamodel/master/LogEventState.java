0b16c4eb822f241a522605a890b27a590db85a00
Francisco Castro <francis.evgc@gmail.com>
2021-03-23 21:38:39 -0400

    Added stateId column

--- src/main/java/org/wlcp/wlcpmetrics/datamodel/master/LogEventState.java
+++ src/main/java/org/wlcp/wlcpmetrics/datamodel/master/LogEventState.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+import javax.persistence.Lob;
 
 import org.wlcp.wlcpmetrics.datamodel.enums.LogEventType;
 
@@ -11,25 +12,47 @@ import org.wlcp.wlcpmetrics.datamodel.enums.LogEventType;
 @Table
 @PrimaryKeyJoinColumn(referencedColumnName = "logEventId")
 public class LogEventState extends LogEvent {
+	
+	@Column
+	private String stateId;
 
 	@Column
+	@Lob
 	private String componentProperties;
 	
 	@Column
 	private String stateEvent;
+	
+	/*
+	 @ElementCollection()
+    @CollectionTable(name = "DISPLAY_TEXT_MAP")
+    @MapKeyColumn(name = "SCOPE")
+    @Column(name = "DISPLAY_TEXT", length = 2048)
+	private Map<String, String> displayText = new HashMap<String, String>();
+	 
+	 */
 	
 
 	public LogEventState() {
 		super(LogEventType.STATE);
 	}
 
-	public LogEventState(String componentProperties, String stateEvent) {
+	public LogEventState(String stateId, String componentProperties, String stateEvent) {
 		//this();
 		super(LogEventType.STATE);
+		this.stateId = stateId;
 		this.componentProperties = componentProperties;
 		this.stateEvent = stateEvent;
 	}
 	
+	
+	public String getStateId() {
+		return stateId;
+	}
+
+	public void setStateId(String stateId) {
+		this.stateId = stateId;
+	}
 
 	public String getComponentProperties() {
 		return componentProperties;
