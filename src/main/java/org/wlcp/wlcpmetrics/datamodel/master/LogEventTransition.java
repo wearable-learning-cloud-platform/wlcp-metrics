1816333e12653de39e7e259a8d90b7792b6ceee1
Francisco Castro <francis.evgc@gmail.com>
2021-03-24 10:06:10 -0400

    Added transitionId and transitionProperties columns

--- src/main/java/org/wlcp/wlcpmetrics/datamodel/master/LogEventTransition.java
+++ src/main/java/org/wlcp/wlcpmetrics/datamodel/master/LogEventTransition.java
@@ -2,6 +2,7 @@ package org.wlcp.wlcpmetrics.datamodel.master;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Lob;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
@@ -12,17 +13,51 @@ import org.wlcp.wlcpmetrics.datamodel.enums.LogEventType;
 @PrimaryKeyJoinColumn(referencedColumnName = "logEventId")
 public class LogEventTransition extends LogEvent {
 	
+	
+	// Columns
+	
+	@Column
+	private String transitionId;
+
+	@Column
+	@Lob
+	private String transitionProperties;
+
 	@Column
 	private String transitionEvent;
+	
+	
+	// Constructors
 	
 	public LogEventTransition() {
 		super(LogEventType.TRANSITION);
 	}
 	
-	public LogEventTransition(String transitionEvent) {
+	public LogEventTransition(String transitionId, String transitionProperties, String transitionEvent) {
 		super(LogEventType.TRANSITION);
 		
+		this.transitionId = transitionId;
+		this.transitionProperties = transitionProperties;
 		this.transitionEvent = transitionEvent;
+	}
+	
+	
+	// Getters and Setters
+	
+	public String getTransitionId() {
+		return transitionId;
+	}
+
+	public void setTransitionId(String transitionId) {
+		this.transitionId = transitionId;
+	}
+	
+	public String getTransitionProperties() {
+		return transitionProperties;
+	}
+
+	public void setTransitionProperties(String transitionProperties) {
+		this.transitionProperties = transitionProperties;
 	}
 	
 	public String getTransitionEvent() {
